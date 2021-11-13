2720fb56f6e0bf01a59a963a9e7f6c026a0c978c
CHINO PROGRAMADOR :) <84936878+ChinoRD@users.noreply.github.com>
2021-11-13 13:45:54 -0400

    Update fxmanifest.lua
    dependencias

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,10 @@
 --- NO VENDER PORFAVOR -- 
-fx_version 'bodacious'
+fx_version 'cerulean'
 game 'gta5'
 
 version '1.0.0'
+
+description 'https://github.com/ChinoRD/ch_vape'
 
 
 client_scripts{
@@ -13,4 +15,10 @@ client_scripts{
 server_scripts{
 	'config/*.lua',
 	'server/*.lua',
+}
+
+dependencies {
+	'qb-core',
+	-- Si quieres cambiar el scripts de framework elemina esta linea de codigo (DESDE LA 20)
+	-- https://github.com/qbcore-framework
 }
