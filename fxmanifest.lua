1c88ea8741d8f3e00b0e444eb57c96f2f74addf2
noxyyfgo <84936878+noxyyfgo@users.noreply.github.com>
2021-11-13 13:21:25 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+--- NO VENDER PORFAVOR -- 
+fx_version 'bodacious'
+game 'gta5'
+
+version '1.0.0'
+
+
+client_scripts{
+	'config/*.lua',
+	'client/*.lua'
+}
+
+server_scripts{
+	'config/*.lua',
+	'server/*.lua',
+}
