b16e1d11100e88dad6f88db21824ae4d8e87b60f
noxyyfgo <84936878+noxyyfgo@users.noreply.github.com>
2021-11-13 13:22:48 -0400

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,29 @@
+Config = {}
+
+Config['DragControl']              = 51         
+
+Config['RestingAnim']              = 58         
+
+Config['ButtonHoldTime']           = 250 	
+
+Config['SmokeSize']                = 0.5 	
+
+Config['FailureOdds']              = 10594  
+
+Config['VapeCoolDownTime']         = 4000  
+
+Config['VapeHangTime']             = 2800  
+
+Config['VapePermission']           = false  
+
+Config['HelpTextStartingAlpha']    = 0      
+
+Config['HelpTextLength']           = 8000    
+
+Config['Bones']                    = {20279,} 
+
+Config['ParticleAsset']            = "core"     
+
+Config['Particle']                 = "exp_grd_bzgas_smoke"
+
+Config['EstresPorFumar']           = 10000
