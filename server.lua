aedb0dc51f3e319b2df936927e375d7acd75fd19
CHINO PROGRAMADOR :) <84936878+ChinoRD@users.noreply.github.com>
2021-11-13 14:37:29 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -5,24 +5,6 @@ local QBCore = exports['qb-core']:GetCoreObject()
 RegisterNetEvent("chino_vape:Fail")
 RegisterServerEvent("chino_vape:Smoke")
 
-QBCore.Functions.CreateCallback('chino_vape:CheckVaper', function(source, cb)
-    local xPlayer    = QBCore.Functions.GetPlayer(source)
-	local result     = 0
-	
-	local vaper      = xPlayer.Functions.GetItemByName('vaper')
-	local vaperb     = xPlayer.Functions.GetItemByName('vaper_blue')
-	local vaperg     = xPlayer.Functions.GetItemByName('vaper_green')
-
-	if (vaper.count > 0) or (vaperb.count > 0) or (vaperg.count > 0) then
-		result = 1
-		cb(result)	
-	else
-		result = 2
-		cb(result)
-	end
-
-	print(xPlayer.Functions.GetItemByName('vaper').count)
-end)
 
 AddEventHandler("chino_vape:Fail", function()
 	TriggerClientEvent('dopeNotify2', source, "ERROR", "<span style='color:#c7c7c7'>Te exploto un <span style='color:#ff0000'>VAPE</span> un vape en la cara!", 5000, 'error')
@@ -34,24 +16,25 @@ end)
 
 -- fin de eventos
 
--- Inicio de los items
+-- Inicio de los items (ESTO ESTA EN PREGRAMACION NO ESTA FUNCIONAL)
 
-QBCore.Functions.CreateUseableItem("vaper_blue", function(source, item)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    TriggerEvent("chino_vape:OpenMenu")
-end)
+--QBCore.Functions.CreateUseableItem("vaper_blue", function(source, item)
+    --local src = source
+    --local Player = QBCore.Functions.GetPlayer(src)
+    --TriggerEvent("chino_vape:OpenMenu")
+--end)
 
-QBCore.Functions.CreateUseableItem("vaper_green", function(source, item)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    TriggerEvent("chino_vape:OpenMenu")
-end)
+--QBCore.Functions.CreateUseableItem("vaper_green", function(source, item)
+    --local src = source
+    --local Player = QBCore.Functions.GetPlayer(src)
+    --TriggerEvent("chino_vape:OpenMenu")
+--end)
 
-QBCore.Functions.CreateUseableItem("vaper", function(source, item)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    TriggerEvent("chino_vape:OpenMenu")
-end)
+--QBCore.Functions.CreateUseableItem("vaper", function(source, item)
+    --local src = source
+    --local Player = QBCore.Functions.GetPlayer(src)
+    --TriggerEvent("chino_vape:OpenMenu")
+--end)
 
 -- fin de los items
+
