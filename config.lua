0a5f72cd968ed7d55a1e114ed8ca9b46ed50a41a
noxyyfgo <84936878+noxyyfgo@users.noreply.github.com>
2021-11-13 13:30:54 -0400

    Update config.lua
    E puesto nuevas funciones y para que sirven

--- config.lua
+++ config.lua
@@ -1,29 +1,35 @@
 Config = {}
 
-Config['DragControl']              = 51         
+Config['DragControl']              = 51         -- tecla para vaper (poner el vape en la boca) 
 
-Config['RestingAnim']              = 58         
+Config['RestingAnim']              = 58         -- telca para resetear la animacion de vapear
 
-Config['ButtonHoldTime']           = 250 	
+Config['ButtonHoldTime']           = 250 	 -- tiempo de precion de tecla
 
-Config['SmokeSize']                = 0.5 	
+Config['SmokeSize']                = 0.5 	-- NO TOCAR almenos que sepas que estas haciendo :)
 
-Config['FailureOdds']              = 10594  
+Config['FailureOdds']              = 10594  -- NO TOCAR almenos que sepas que estas haciendo :)
 
-Config['VapeCoolDownTime']         = 4000  
+Config['VapeCoolDownTime']         = 4000  -- NO TOCAR almenos que sepas que estas haciendo :)
 
-Config['VapeHangTime']             = 2800  
+Config['VapeHangTime']             = 2800  -- NO TOCAR almenos que sepas que estas haciendo :)
 
-Config['VapePermission']           = false  
+Config['VapePermission']           = false  -- NO TOCAR almenos que sepas que estas haciendo :)
 
-Config['HelpTextStartingAlpha']    = 0      
+Config['HelpTextStartingAlpha']    = 0      -- NO TOCAR almenos que sepas que estas haciendo :)
 
-Config['HelpTextLength']           = 8000    
+Config['HelpTextLength']           = 8000   -- NO TOCAR almenos que sepas que estas haciendo :) 
 
-Config['Bones']                    = {20279,} 
+Config['Bones']                    = {20279,} -- NO TOCAR almenos que sepas que estas haciendo :)
 
-Config['ParticleAsset']            = "core"     
+Config['ParticleAsset']            = "core"     -- NO TOCAR almenos que sepas que estas haciendo :)
 
-Config['Particle']                 = "exp_grd_bzgas_smoke"
+Config['Particle']                 = "exp_grd_bzgas_smoke" -- NO TOCAR almenos que sepas que estas haciendo :)
 
-Config['EstresPorFumar']           = 10000
+-- puedes encontrar las tecla aqui tienes que poner index o el numero https://docs.fivem.net/docs/game-references/controls/
+
+-- ejemplo: TECLA E
+
+-- 38	INPUT_PICKUP	E	LB
+
+-- el index o numero de la tecla es el 38.
